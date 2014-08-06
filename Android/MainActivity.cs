d1679ba9d59749d8f04703e3f6a10255bfc711dc
bootstraponline <code@bootstraponline.com>
2014-08-06 00:35:16 -0400

    remove reflection code

--- Android/MainActivity.cs
+++ Android/MainActivity.cs
@@ -8,7 +8,6 @@ using Android.Widget;
 using Android.OS;
 
 using Xamarin.Forms.Platform.Android;
-using System.Reflection;
 
 namespace ruby_lib.Android
 {
@@ -18,17 +17,6 @@ namespace ruby_lib.Android
         protected override void OnCreate (Bundle bundle)
         {
             base.OnCreate (bundle);
-
-            Map<String,Integer> stringIds = new Map<String, Integer>();
-            FieldInfo[] fields = Resource.String.GetType().GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
-            foreach (FieldInfo field in fields) {
-                if (!field.IsInitOnly && field.IsLiteral) {
-                    stringIdsMap.add (field, field.GetValue(null));
-                }
-            }
-
-
-            Context.Resources.GetString (Resource.String.hello);
 
             Xamarin.Forms.Forms.Init (this, bundle);
 
